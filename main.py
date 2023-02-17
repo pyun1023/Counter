fb7ab8ccba7a6113e14279b364bca48581bc8df3
pyun1023 <pyun1023@gmail.com>
2023-02-17 12:13:34 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count'] =0
+    else
+        session['count'] +=1
+    return render_template('index.html', count=session['count'])
+
+@app.route('/two', methods=['POST'])
+def add_two():
+    session['count'] +=1
+    return redirect('/')
+
+@app.route('/destroy_session', methods=['POST'])
+def delete():
+    session['count']=0
+    return redirect('/')
+
+app.run(debug=True)
